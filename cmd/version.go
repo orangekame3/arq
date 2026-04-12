a5a8fb548b40f4f95ed3304dc67116a12f1d82a7
Takafumi Miyanaga <u315098d@icho2.osaka-u.ac.jp>
2026-04-12 15:36:08 +0900

    [tagpr] prepare for the next release

--- cmd/version.go
+++ cmd/version.go
@@ -8,7 +8,7 @@ import (
 
 // Version is the current version of arq.
 // tagpr will update this value automatically.
-var Version = "0.0.5"
+var Version = "0.0.6"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
